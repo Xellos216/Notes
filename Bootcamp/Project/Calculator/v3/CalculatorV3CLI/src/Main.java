6d7f4d49afb453f961fc9e20e0b7c5762b361f5b
H <xellos216@gmail.com>
2025-07-14 14:29:36 +0900

    refactoring

---
+++ Bootcamp/Project/Calculator/v3/CalculatorV3CLI/src/Main.java
@@ -0,0 +1,41 @@
+import service.ArithmeticCalculator;
+import model.ArithmeticOperator;
+import ui.CalculatorUI;
+import util.SafeRunner;
+
+public class Main {
+    public static final boolean DEBUG = true;
+
+    public static void main(String[] args) {
+        CalculatorUI ui = new CalculatorUI();
+        ArithmeticCalculator calculator = new ArithmeticCalculator();
+
+        ui.printwelcome();
+
+        while (true) {
+            Double a = ui.readNumber("Enter first number (or 'exit'):");
+            if (a == null) break;
+
+            ArithmeticOperator op = ui.readOperator("Enter operator (+, -, *. /):");
+            if (op == null) continue;
+
+            Double b = ui.readNumber("Enter second number (or 'exit):");
+            if (b == null) breake;
+
+            try {
+                double result = calculator.calculate(a, b, op);
+                ui.printResult(result);
+
+                if (DEBUG) {
+                    ui.printDebug(calculator);
+                }
+            }catch (Exception e) {
+                ui.printError("Calculation error: " + e.getMessage());
+            }
+
+            ui.printHistory(calculator.getResults());
+        }
+
+        ui.printExit();
+    }
+}
